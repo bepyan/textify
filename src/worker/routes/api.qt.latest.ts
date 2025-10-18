8eebcfa5ee793853b10cdf39211bebfbc94fc226
bepyan <bepyan@naver.com>
2025-10-19 01:49:52 +0900

    feat: text/plain 응답으로 변경

--- src/worker/routes/api.qt.latest.ts
+++ src/worker/routes/api.qt.latest.ts
@@ -6,13 +6,8 @@ import { extractQtLatest } from '@be/lib/qt-extractor';
 // Schema
 // ============================================================================
 
-const ResponseSchema = z.object({
-  title: z.string().openapi({
-    example: '성전의 정화',
-  }),
-  description: z.string().openapi({
-    example: '본문 : 역대하(2 Chronicles)29:1 - 29:19 찬송가 14장',
-  }),
+const ResponseSchema = z.string().openapi({
+  example: '성전의 정화\n본문 : 역대하(2 Chronicles)29:1 - 29:19 찬송가 14장',
 });
 
 // ============================================================================
@@ -26,11 +21,19 @@ const route = createRoute({
   responses: {
     200: {
       content: {
-        'application/json': {
+        'text/plain': {
           schema: ResponseSchema,
         },
       },
       description: '매일성경 본문 추출',
+    },
+    500: {
+      content: {
+        'text/plain': {
+          schema: z.string(),
+        },
+      },
+      description: 'Failed to extract daily Bible text',
     },
   },
 });
@@ -43,7 +46,12 @@ const app = new OpenAPIHono()
   ///////////////////////////////////////////////////////////////////////////////
   .openapi(route, async (c) => {
     const result = await extractQtLatest();
-    return c.json(result, 200);
+
+    if (!result.title || !result.description) {
+      return c.text('Failed to extract daily Bible text', 500);
+    }
+
+    return c.text(result.title + '\n' + result.description, 200);
   });
 
 export default app;
